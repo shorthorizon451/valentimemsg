c9d8f6d48c72f4fd027856e796021dc1dd3d6a8c
short_horizon451/caleb <115152760+shorthorizon451@users.noreply.github.com>
2023-02-11 18:24:04 +0800

    Add files via upload

---
+++ ValentimeMSG/valentime.bat
@@ -0,0 +1,14 @@
+@echo off 
+color a
+start valentimehta.hta
+echo ================================================================================
+echo ============= ValenTimeMSG v.1.1 (Copyright HorizonCorp 2021-2023) =============
+echo ================================================================================
+echo =   Longest Running Beta Testing of HorizonCorp since 2021 (2nd Beta Release)  =
+echo ================================================================================
+echo = Github Repository: https://www.github.com/shorthorizon451                    =
+echo = Main Developer: Caleb (ShortHorizon451)                                      =
+echo = Demonstration of: Microsoft Notepad for Windows' versatility on code editing =
+echo ================================================================================
+echo = Ran on: %date% %time%                                               =
+echo ================================================================================
